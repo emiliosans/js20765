5da6504f52bd91ef4c7caa96858258480bc79220
Tardes <tardes@webex.local>
2020-12-23 20:18:24 +0100

    ulitmo dia

---
+++ ubicacion.js
@@ -0,0 +1,65 @@
+let divout = null;
+let mapa = null;
+
+const LATITUD_MADRID = 40.4167;
+const LONGITUD_MADRID = -3.70325;
+
+let mapa_dibujado = false;
+
+function dibujarUbicacion(latitud, longitud) {
+    if (mapa_dibujado) {
+        //dibujar en el mapa de google un marcador
+        let pos_actual = { lat: latitud, lng: longitud };
+
+        let marcador = new google.maps.Marker(
+            {
+                position: pos_actual,
+                title: "Estás aquí"
+            }
+        );
+
+        marcador.setMap(mapa);
+
+    } else {
+        //boton deshabiltado y habilitado al recibir el mapa
+        alert ("Mapa no disponible");
+    }
+
+}
+
+function initMap() {
+    mapa = new google.maps.Map(
+        document.getElementById("map"),
+        {
+            center: { lat: LATITUD_MADRID, lng: LONGITUD_MADRID },
+            zoom: 10
+        }
+    );
+    mapa_dibujado = true;
+}
+
+function encuentrame() {
+    divout = document.getElementById("out");
+    if (!navigator.geolocation) {
+        console.log("Estoy en un navegador antiguo");
+        console.log("que no tiene API GEOLOCATION!");
+        fallo();
+    }
+    else {
+        console.log("Estoy en un navegador moderno");
+        console.log("Sí tiene API GEOLOCATION!");
+        console.log("preguntamos...");
+        navigator.geolocation.getCurrentPosition(exito, fallo);
+        divout.innerHTML = "Buscando ...";
+    }
+}
+function exito(posicion) {
+    let latitud = posicion.coords.latitude;
+    let longitud = posicion.coords.longitude;
+    divout.innerHTML = "Latitud = " + latitud + "Longitud = " + longitud;
+    dibujarUbicacion(latitud, longitud);
+}
+function fallo() {
+    console.log("No esta disponible la ubicación");
+    divout.innerHTML = "No esta disponible la ubicación";
+}
