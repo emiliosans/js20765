06221dce56f6a67303fea4180ed6fcb99eb56596
vale <vale@debian.sin>
2021-01-25 16:05:30 +0100

    plugin network info jc

--- EjPluginRed/www/js/index.js
+++ EjPluginRed/www/js/index.js
@@ -21,9 +21,12 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+
+
 }
